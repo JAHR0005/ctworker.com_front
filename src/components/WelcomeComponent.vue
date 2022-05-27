cb967947bef7a7eac7ac4a79583d12d35e627d02
JAHR0005 <jahr0005@gmail.com>
2022-05-27 13:37:00 +0200

    first commit

---
+++ src/components/WelcomeComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1 class="text-center my-5 title text-muted"><span class="text-success">Ct</span>Worker</h1>
+    <b-col class="search-input mx-auto">
+        <b-icon  icon="search" class="search-icon"></b-icon>
+        <b-form-input @keyup.enter="hola()"></b-form-input> 
+    </b-col>
+  </div>
+</template>
+<script>
+export default {
+    name: 'WelcomeComponent',
+    methods:{
+        hola(){
+            alert('hola')
+        }
+    }
+}
+</script>
+<style scoped>
+.form-control{
+    border-radius: 180px;  
+    height: 48px;
+    padding-left: 60px;
+    padding-right: 30px
+}
+.search-input{
+    width:600px!important;
+}
+.form-control:focus{
+    outline:0!important
+}
+.title{
+    font-size: 4rem;
+    font-weight: 800;
+}
+.search-icon{
+    position:absolute;
+    z-index: 10;
+    margin-left: 20px;
+    font-size: 20px;
+    margin-top: 12px;
+    color:grey
+}
+</style>
